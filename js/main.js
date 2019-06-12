2954ac077fd5c3b018024b5eda611ff5b59f7672
arnovanstaden <arnovanstaden@gmail.com>
2019-06-12 11:29:13 +0200

    finish carousel

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Navbar
 
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
@@ -11,34 +11,47 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-function openNav()  {
-    $(".navbar-overlay").css("height","100%");
-    setTimeout( function() {
-        $(".navbar-overlay-logo").css("opacity","0.1");
-        $(".navbar-overlay-heading").css("opacity","1");
-        $(".navbar-overlay-line").css("height","25vh");
-        $(".navbar-close").css("opacity","1");
-    }, 1000);
-    setTimeout( function() {
-        $(".navbar-overlay-menu").css("opacity","1");
-        $(".navbar-overlay-menu a").css("left","0");
-    }, 2000);
+function openNav() {
+  $(".navbar-overlay").css("height", "100%");
+  setTimeout(function () {
+    $(".navbar-overlay-logo").css("opacity", "0.1");
+    $(".navbar-overlay-heading").css("opacity", "1");
+    $(".navbar-overlay-line").css("height", "25vh");
+    $(".navbar-close").css("opacity", "1");
+  }, 1000);
+  setTimeout(function () {
+    $(".navbar-overlay-menu").css("opacity", "1");
+    $(".navbar-overlay-menu a").css("left", "0");
+  }, 2000);
 }
 
-function closeNav()  {
-    $(".navbar-overlay-logo").css("opacity","1");
-    $(".navbar-overlay-heading").css("opacity","0");
-    $(".navbar-overlay-line").css("height","0");
-    $(".navbar-close").css("opacity","1");
-    $(".navbar-overlay-menu").css("opacity","0");
-    $(".navbar-overlay-menu a").css("left","-150%");
-    setTimeout( function() {
-          $(".navbar-overlay").css("height","0");
-          $(".navbar-overlay-logo").css("opacity","1");
-    }, 1500);
+function closeNav() {
+  $(".navbar-overlay-logo").css("opacity", "1");
+  $(".navbar-overlay-heading").css("opacity", "0");
+  $(".navbar-overlay-line").css("height", "0");
+  $(".navbar-close").css("opacity", "1");
+  $(".navbar-overlay-menu").css("opacity", "0");
+  $(".navbar-overlay-menu a").css("left", "-150%");
+  setTimeout(function () {
+    $(".navbar-overlay").css("height", "0");
+    $(".navbar-overlay-logo").css("opacity", "1");
+  }, 1500);
 }
 
-// White Background on video end
-$('#myVideo').on('ended',function(){
-    $('#myVideo').fadeOut(1000);
-  });
+
+
+// Home Page Owl Carousel
+var owl = $('.owl-carousel');
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 1,
+  items: 1,
+  autoplay: true,
+  slideTransition: "ease",
+  smartSpeed: 1500
+});
+
+
+setInterval(()=> {
+  owl.trigger('next.owl.carousel');
+}, 5000);
