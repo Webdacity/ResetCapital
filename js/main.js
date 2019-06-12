9454739266ba05d18f8adeda05221cbd5abb77bd
arnovanstaden <arnovanstaden@gmail.com>
2019-06-12 13:09:45 +0200

    finsish animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// Page Loader
+
+// setTimeout (function() {
+//   $(".page-loader").fadeOut("2000");
+// }, 5000);
+
+
 // Navbar
 
 var prevScrollpos = window.pageYOffset;
@@ -52,6 +59,122 @@ $('.owl-carousel').owlCarousel({
 });
 
 
-setInterval(()=> {
+setInterval(() => {
   owl.trigger('next.owl.carousel');
-}, 5000);
+}, 6000);
+
+
+
+// ScrollReveal Animations 
+
+window.sr = ScrollReveal({
+  reset: false,
+  mobile: true
+});
+
+ScrollReveal().reveal('.about-section .black-line:nth-child(2)', {
+  duration: 1500,
+  distance: "1000px",
+  origin: "right"
+});
+
+ScrollReveal().reveal('.about-section .black-line:nth-child(3)', {
+  duration: 2000,
+  distance: "1000px",
+  origin: "left"
+});
+
+ScrollReveal().reveal('.contact-section .black-line:nth-child(3)', {
+  duration: 1500,
+  distance: "1000px",
+  origin: "right"
+});
+
+ScrollReveal().reveal('.contact-section .black-line:nth-child(2)', {
+  duration: 2000,
+  distance: "1000px",
+  origin: "left"
+});
+
+ScrollReveal().reveal('.owl-carousel', {
+  duration: 2500
+});
+
+ScrollReveal().reveal('.navbar', {
+  duration: 2500,
+  distance: "400px",
+  origin: "top",
+});
+
+ScrollReveal().reveal('.about-section-heading, .funders-section-heading', {
+  duration: 2000,
+  distance: "400px",
+  origin: "left",
+});
+
+ScrollReveal().reveal('.investments-section-heading, .contact-section-heading', {
+  duration: 2000,
+  distance: "400px",
+  origin: "right",
+});
+
+ScrollReveal().reveal('.about-text p:nth-child(1)', {
+  duration: 2000,
+  distance: "1000px",
+  origin: "right",
+});
+
+ScrollReveal().reveal('.about-text div', {
+  duration: 2000,
+  distance: "1000px",
+  origin: "right",
+  delay: 300
+});
+
+ScrollReveal().reveal('.about-text p:nth-child(4)', {
+  duration: 2000,
+  distance: "1000px",
+  origin: "right",
+  delay: 600
+});
+
+ScrollReveal().reveal('.black-divider-line, orange-running-lin, .about-section .orange-running-line+h1', {
+  duration: 2000,
+  distance: "200px",
+  origin: "bottom",
+});
+
+ScrollReveal().reveal('.team-grid .team-member:nth-child(1)', {
+  duration: 2000,
+  distance: "1000px",
+  origin: "left",
+});
+
+ScrollReveal().reveal('.team-grid .team-member:nth-child(2)', {
+  duration: 2000,
+  distance: "1000px",
+  origin: "right",
+});
+
+ScrollReveal().reveal('.contact-details', {
+  duration: 2000,
+  distance: "1000px",
+  origin: "left",
+});
+
+ScrollReveal().reveal('.contact-section .orange-line', {
+  duration: 2000,
+  distance: "1000px",
+  origin: "left",
+  delay: 300
+});
+
+ScrollReveal().reveal('.contact-form', {
+  duration: 2000,
+  distance: "1000px",
+  origin: "left",
+  delay: 600
+});
+
+
+
